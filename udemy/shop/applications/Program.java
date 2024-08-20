5ebcf8ff0cde04187368dca7ec82faeff0b81781
israel camilo <abeatrizcamilo@gmail.com>
2024-08-20 12:08:28 -0300

    using classe methods

--- udemy/shop/applications/Program.java
+++ udemy/shop/applications/Program.java
@@ -20,6 +20,8 @@ public class Program{
        System.out.print("Quantity in stock: ");
        product.quantity = sc.nextInt();
 
+       System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+
        sc.close();
     }
 }
