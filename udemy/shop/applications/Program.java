8ede72d61a5b9fd2b5f3c538505a6ebb19572d68
israel camilo <abeatrizcamilo@gmail.com>
2024-08-30 19:35:07 -0300

    String.format(%.2f, price) -> toString

--- udemy/shop/applications/Program.java
+++ udemy/shop/applications/Program.java
@@ -20,7 +20,9 @@ public class Program{
        System.out.print("Quantity in stock: ");
        product.quantity = sc.nextInt();
 
-       System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+       
+
+       System.out.println("Product data: " + product);
 
        sc.close();
     }
