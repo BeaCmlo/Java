98ca372000daea4bf3d6d18671eacc3019bcd986
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-06 22:19:25 -0300

    live

--- lives/src/tests.java
+++
@@ -1,9 +0,0 @@
-public class tests {
-    public static void main(String[] args) {
-
-        String game1 = "Dead By Daylight";
-        String game2 = "Subway Surf";
-
-        System.out.println(game2.compareTo(game1));
-    }
-}
