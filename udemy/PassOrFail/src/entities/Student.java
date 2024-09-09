90f74fb59139f3ad9efb5d8d0907fa28f0f33728
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-09 15:56:25 -0300

    class project

---
+++ udemy/PassOrFail/src/entities/Student.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Student {
+    public String name;
+    public double av1, av2, av3;
+    private double avarage;
+    private boolean pass;
+    private double complement;
+
+    public boolean avarege() {
+        avarage = (((av1 * 30) + (av2 * 35) + (av3*35))/100);
+        complement = (60 - avarage);
+        if(avarage > 60){
+            pass = true;
+        }else{
+            pass = false;
+        }
+        return pass;
+    }
+
+    @Override
+    public String toString() {
+        if (pass){
+            return "Student: "+ name + "\nFINAL GRADE = " + String.format("%.2f", avarage) + "\nPASS";
+        }else{
+            return "Student: "+ name + "\nFINAL GRADE = " + String.format("%.2f", avarage) + "\nFAILED\nMISSING " + String.format("%.2f", complement) + " POINTS";
+        }
+    }
+}
