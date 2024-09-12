68cd2f87b29f1f870c4c54886695210899c5e320
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-12 18:12:54 -0300

    Implementing construstors

--- udemy/shop/entities/Product.java
+++ udemy/shop/entities/Product.java
@@ -3,7 +3,13 @@ package udemy.shop.entities;
 public class Product {
     public String name;
     public double price;
-    public int quantity; 
+    public int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name; // Referência "this" ao atrinuto do objeto
+        this.price = price; // Referência "this" ao atrinuto do objeto
+        this.quantity = quantity; // Referência "this" ao atrinuto do objeto
+    }
 
 
     public double totalValueInStock(){
