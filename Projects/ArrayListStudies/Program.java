b6c90f8798e1e49374f35f41c3087a631cdf3d48
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-15 17:42:09 -0300

    Using Maps and ArrayLists

--- Projects/ArrayListStudies/Program.java
+++ Projects/ArrayListStudies/Program.java
@@ -3,51 +3,6 @@ package Projects.ArrayListStudies;
 import java.util.*;
 
 public class Program {
-    public static class Bakery{
-        private String name;
-        private int numsOfBreads;
-        private int numsOfDrinks;
-        private ArrayList<String> breads;
-        private ArrayList<String> drinks;
-        private Map<String, Double> priceBreads;
-        private Map<String, Double> priceDrinks;
-        private Map<Map<String,Double>, Map<String,Double>> priceBreadsAndDrinks;
-
-
-        public Bakery(String name, int numsOfBreads, int numsOfDrinks, ArrayList<String> breads, ArrayList<String> drinks) {
-            this.name = name;
-            this.breads = breads;
-            this.drinks = drinks;
-            this.numsOfBreads = numsOfBreads;
-            this.numsOfDrinks = numsOfDrinks;
-            this.priceBreads = new HashMap<>();
-            this.priceDrinks = new HashMap<>();
-        }
-
-        public Map<String, Double> getPriceBreads() {
-
-            return priceBreads;
-        }
-        public Map<String, Double> getPriceDrinks() {
-            return priceDrinks;
-        }
-        public Map<Map<String, Double>, Map<String, Double>> getPriceBreadsAndDrinks() {
-
-            return priceBreadsAndDrinks;
-        }
-
-
-        @Override
-        public String toString() {
-            return "Bakery{" +
-                    "name='" + name + '\'' +
-                    ", breads=" + breads +
-                    ", drinks=" + drinks +
-                    ", menu=" +
-                    '}';
-        }
-    }
-
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
@@ -64,7 +19,7 @@ public class Program {
         for (int i = 0; i < numsOfBreads; i++) {
             breads.add(sc.nextLine());
         }
-        /*for (int i = 0; i < numsOfBreads; i++) {
+        /*for (int i = 0; i < numsOfBreads; i++) { // Debug
             System.out.print(breads.get(i));
             if (i != numsOfBreads - 1) {
                 System.out.print(", ");
