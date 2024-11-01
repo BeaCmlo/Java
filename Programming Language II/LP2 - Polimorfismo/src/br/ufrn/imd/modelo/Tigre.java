7d86921a7792f2a2c6d014abfeed1735a245fc4a
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-10-31 21:13:30 -0300

    Class Project

--- Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/modelo/Tigre.java
+++ Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/modelo/Tigre.java
@@ -1,4 +1,31 @@
 package br.ufrn.imd.modelo;
 
 public class Tigre {
+    private String tipoTigre;
+
+
+
+    public Tigre() {
+        super();
+        tipoTigre = "";
+    }
+
+
+
+    public String getTipoTigre() {
+        return tipoTigre;
+    }
+
+
+
+    public void setTipoTigre(String tipoTigre) {
+        this.tipoTigre = tipoTigre;
+    }
+
+
+
+    public void comer() {
+
+    }
+
 }
