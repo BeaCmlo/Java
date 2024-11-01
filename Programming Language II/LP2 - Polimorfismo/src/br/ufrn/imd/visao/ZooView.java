0e41d79f0a595739f8ebba3392e946c9ed6a2e71
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-10-31 21:13:38 -0300

    Class Project

--- Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/visao/ZooView.java
+++ Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/visao/ZooView.java
@@ -1,8 +1,26 @@
 package br.ufrn.imd.visao;
 
+import br.ufrn.imd.modelo.Girafa;
+import br.ufrn.imd.dao.BancoDeDados;
 public class ZooView {
 
     public static void main(String[] args) {
+
+        Girafa girafa1 = new Girafa();
+        girafa1.setNome("Arta");
+        girafa1.setPeso(140);
+        girafa1.setAlimentacao("Maca");
+        girafa1.setAlimentado(true);
+        girafa1.setTamanoLingua(10);
+        //girafa1.setDataNascimento();
+        girafa1.comer();
+
+        BancoDeDados bancoDeDados = new BancoDeDados();
+
+        bancoDeDados.adionarAnimal(girafa1);
+        bancoDeDados.exibirAnimaisCadastrados();
+        bancoDeDados.exibirAlimentosTotais();
+
 
     }
 }
