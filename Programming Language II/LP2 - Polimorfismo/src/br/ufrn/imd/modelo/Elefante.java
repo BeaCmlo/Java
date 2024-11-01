1cce011d0c4480b96e0ea1f4d9948dec0b3bd0d2
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-10-31 21:13:11 -0300

    Class Project

--- Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/modelo/Elefante.java
+++ Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/modelo/Elefante.java
@@ -1,4 +1,26 @@
 package br.ufrn.imd.modelo;
 
-public class Elefante {
+public class Elefante extends Animal {
+    private int tamanhoTromba;
+
+
+
+    public int getTamanhoTromba() {
+        return tamanhoTromba;
+    }
+
+
+
+    public void setTamanhoTromba(int tamanhoTromba) {
+        this.tamanhoTromba = tamanhoTromba;
+    }
+
+    public Elefante() {
+        super();
+        tamanhoTromba = 0;
+    }
+
+    public void comer(){
+        quantidadeAlimento = peso * (15/100);
+    }
 }
