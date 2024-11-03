f8c2a08df4521bc83f32a84ec2d4a58063807ed4
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-11-03 18:51:28 -0300

    correction

--- Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/visao/ZooView.java
+++ Programming Language II/LP2 - Polimorfismo/src/br/ufrn/imd/visao/ZooView.java
@@ -19,17 +19,23 @@ public class ZooView {
         girafa1.setAlimentacao("Maca");
         girafa1.setAlimentado(true);
         girafa1.setTamanoLingua(10);
-        Date date = new Date(1970, 10,03);
-        girafa1.setDataNascimento(date);
+        Date date1 = new Date(1970, 10,03);
+        girafa1.setDataNascimento(date1);
+        girafa1.calcularIdade();
         girafa1.comer();
 
         Tigre tigre1 = new Tigre();
         tigre1.setNome("Fofao");
         tigre1.setPeso(140);
         tigre1.setAlimentacao("Carne");
+        Date date2 = new Date(1990, 12,01);
+        tigre1.setDataNascimento(date2);
+        tigre1.calcularIdade();
         tigre1.setAlimentado(true);
         tigre1.setTipoTigre("Malhado");
         tigre1.comer();
+
+
 
         BancoDeDados bancoDeDados = new BancoDeDados();
 
@@ -39,5 +45,6 @@ public class ZooView {
         bancoDeDados.consultar(girafa1);
         bancoDeDados.exibirAlimentosTotais();
 
+        bancoDeDados.listarIdade();
     }
 }
